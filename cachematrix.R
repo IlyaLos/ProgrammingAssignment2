8a4f4f0d4aca4997d343bf20b24621246bb1eef4
Ilya Los <ilos@griddynamics.com>
2017-05-11 12:14:01 +0300

    Commit all changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# this function create object with matrix, set and get methods for it
+# also it contains cached value of inverse matrix with set and get methods for it
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    result <- NULL
+    set <- function(y) {
+        x <<- y
+        result <<- NULL #drop cache if matrix change
+    }
+    get <- function() x
+    setinverse <- function(inverse) result <<- inverse
+    getinverse <- function() result
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+#firstly, function check cache in x. if there is no cache, then call function solve for matrix
+#and cached calculated matix. otherwise get matrix from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    result <- x$getinverse()
+    if (is.null(result)) {
+        result <- solve(x$get())
+        x$setinverse(result)
+    } else {
+        message("getting cached data")
+    }
+    
+    result
 }
